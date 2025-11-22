f48ff26e61983ecf459445109e8c0f487d82db48
Tanvirgit07 <ahmmedtanvir009@gmail.com>
2025-11-22 15:03:02 +0600

    add loding

---
+++ src/app/(website)/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-white/60">
+      <div className="animate-spin h-10 w-10 border-4 border-gray-300 border-t-blue-600 rounded-full"></div>
+    </div>
+  );
+}
